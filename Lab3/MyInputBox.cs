2ab4ab3a9d9c237d7d7caf2065b988804c350d1d
Polliakov <87089375+Polliakov@users.noreply.github.com>
2021-10-22 17:52:29 +0300

    Тестовый коммит.

--- Lab3/MyInputBox.cs
+++ Lab3/MyInputBox.cs
@@ -12,7 +12,7 @@ namespace KurgasovLabs.Lab3
 
         private void button1_Click(object sender, EventArgs e)
         {
-            var inputBox = new Interaction.InputBox("Введите имя.");
+            var inputBox = new Interaction.InputBox("Введите имя, пожалуйста.");
 
             if (inputBox.ShowDialog() == DialogResult.OK)
             {
