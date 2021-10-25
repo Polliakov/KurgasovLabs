6e5ffa039f08b5ee4d9250915d8db310bb1b697c
polyakov <anton.pollyakov@gmail.com>
2021-10-25 18:50:05 +0300

    Добалено Pension.

--- Lab5/Pension.cs
+++ Lab5/Pension.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace KurgasovLabs.Lab5
@@ -15,6 +8,34 @@ namespace KurgasovLabs.Lab5
         public Pension()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(textBox1.Text, out int startAge) |
+                !int.TryParse(textBox2.Text, out int endAge) |
+                !decimal.TryParse(textBox3.Text, out decimal contribution) |
+                !decimal.TryParse(textBox4.Text, out decimal rate))
+            {
+                MessageBox.Show("Ошибка ввода!");
+                return;
+            }
+
+            textBox5.Text = "Возраст  Сумма вложений, Р  Сумма на счёте, Р  Прибыль, Р" +
+                              Environment.NewLine;
+
+            decimal contributionSum = 0;
+            decimal depositSum = 0;
+            for (int age = startAge; age <= endAge; age++)
+            {
+                contributionSum += contribution;
+                depositSum = contributionSum * (1 + rate / 100);
+                decimal profit = depositSum - contributionSum;
+
+                textBox5.Text += string.Format("{0,-9}{1,-19}{2,-19}{3,-10}" + 
+                    Environment.NewLine,
+                    age, contributionSum, depositSum, profit);
+            }
         }
     }
 }
