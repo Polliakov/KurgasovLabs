b87d5d7bded3e96540f04263cb5275e2954de3f5
polyakov <anton.pollyakov@gmail.com>
2021-10-25 17:18:24 +0300

    Рефакторинг FrameworkMath.

--- Lab3/FrameworkMath.Designer.cs
+++ Lab3/FrameworkMath.Designer.cs
@@ -38,24 +38,27 @@ namespace KurgasovLabs.Lab3
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(34, 9);
+            this.label1.Location = new System.Drawing.Point(11, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(155, 17);
+            this.label1.Size = new System.Drawing.Size(121, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Возведение в степень";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(37, 32);
+            this.textBox1.Location = new System.Drawing.Point(14, 24);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(152, 22);
+            this.textBox1.Size = new System.Drawing.Size(115, 20);
             this.textBox1.TabIndex = 1;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(53, 60);
+            this.button1.Location = new System.Drawing.Point(29, 48);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(121, 23);
+            this.button1.Size = new System.Drawing.Size(84, 23);
             this.button1.TabIndex = 2;
             this.button1.Text = "Вычислить";
             this.button1.UseVisualStyleBackColor = true;
@@ -63,9 +66,10 @@ namespace KurgasovLabs.Lab3
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(53, 89);
+            this.button2.Location = new System.Drawing.Point(29, 75);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(121, 23);
+            this.button2.Size = new System.Drawing.Size(84, 23);
             this.button2.TabIndex = 3;
             this.button2.Text = "Выход";
             this.button2.UseVisualStyleBackColor = true;
@@ -73,13 +77,14 @@ namespace KurgasovLabs.Lab3
             // 
             // FrameworkMath
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(216, 140);
+            this.ClientSize = new System.Drawing.Size(142, 105);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FrameworkMath";
             this.Text = "FrameworkMath";
             this.ResumeLayout(false);
--- Lab3/FrameworkMath.cs
+++ Lab3/FrameworkMath.cs
@@ -1,11 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// КОМЕНТАРИИ УДАЛИТЬ ПЕРЕД ОТПРАВКОЙ
+
+using System;
+// Не нужные using желательно удалять.
+//using System.Collections.Generic;
+//using System.ComponentModel;
+//using System.Data;
+//using System.Drawing;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace KurgasovLabs.Lab3
@@ -19,16 +22,45 @@ namespace KurgasovLabs.Lab3
 
         private void button1_Click(object sender, EventArgs e)
         {
-            double x = Convert.ToInt32(textBox1.Text);
-            double result = Math.Pow(x, 2);
-            string result_str = Convert.ToString(result);
-            MessageBox.Show(result_str, "Результат");
+            //double x = Convert.ToInt32(textBox1.Text);
 
+            /* Не отлавливается RuntimeExeption вызываемое Convert.ToInt32() при некорректном вводе строки.
+             * Для решения проблеммы можно использовать метод int.TryParse() или блоки try{} catch{}.
+             * 
+             * Приведение к int значения, предпологаемо типа double ( Convert.ToInt32() ).
+             * Следует использовать Convert.ToDouble() или double.TryParse().
+             */
+            if (!double.TryParse(textBox1.Text, out double x))
+            {
+                MessageBox.Show("Ошибка ввода!");
+                return;
+            }
+
+            double result = Math.Pow(x, 2);
+
+            //string result_str = Convert.ToString(result);
+            //MessageBox.Show(result_str, "Результат");
+
+            /* Ненужное объявление переменной result_str, переменная используется только 1 раз для вывода.
+             * 
+             * Примечание: Convert.ToString(result) и result.ToString() не имеют существенных различий,
+             * второй вариант имеет боле краткую форму и более читаем.
+             * 
+             * Нарушение нотации именования переменных. 
+             * Для локальных переменных нотация camelCase.
+             */
+            MessageBox.Show(result.ToString(), "Результат");
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             Close();
         }
+
+        /* Графическая часть:
+         * Не нужно жмыхать кнопки (Зошто?).
+         * *Использование выравнивания упрощает создание ровного интерфейса,
+         * верхняя панель при выборе нескольких элементов.
+         */
     }
 }
