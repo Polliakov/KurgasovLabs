6de4d35ff3146d6a313e3360cf2168d9c4da313c
polyakov <anton.pollyakov@gmail.com>
2021-11-11 22:16:11 +0300

    Рефакторинк DigitalClock.

--- Lab5/DigitalClock.cs
+++ Lab5/DigitalClock.cs
@@ -1,94 +1,56 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace KurgasovLabs.Lab5
 {
     public partial class DigitalClock : Form
     {
-        int m, s, ms;
+        private int m;
+        private int s;
+
         public DigitalClock()
         {
             InitializeComponent();
+
             timer1.Interval = 500;
             m = 0;
             s = 0;
-            ms = 0;
             label1.Text = "00";
             label2.Text = "00";
-            label3.Visible = true;
-
-        }
-
-        private void label3_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             m = 0;
             s = 0;
-            ms = 0;
             label2.Text = "00";
             label1.Text = "00";
+            label3.Visible = true;
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
             if (label3.Visible)
             {
-                if (s < 59)
-                {
-                    s++;
-                    if (s < 10)
-                        label2.Text = "0" + s.ToString();
-                    else
-                        label2.Text = s.ToString();
-                }
-                else
-                {
-                    if (m < 59)
-                    {
-                        m++;
-                        if (m < 10)
-                            label1.Text = "0" + m.ToString();
-                        else
-                            label1.Text = m.ToString();
-                        s = 0;
-                        label2.Text = "00";
-                    }
-                    else
-                    {
-                        m = 0;
-                        label1.Text = "00";
-                    }
-
-                }
                 label3.Visible = false;
+                return;
             }
-            else
+
+            s++;
+            if (s == 60)
             {
-                label3.Visible = true;
+                s = 0;
+                m++;
+                if (m == 60)
+                {
+                    m = 0;
+                    s = 0;
+                }
+                label1.Text = m.ToString("00");
             }
 
-
-        }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-
+            label2.Text = s.ToString("00");
+            label3.Visible = true;
         }
 
         private void button1_Click(object sender, EventArgs e)
