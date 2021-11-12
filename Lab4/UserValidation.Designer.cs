0cda21eaa2c2a2cf09362f465ef313e4d5698546
polyakov <anton.pollyakov@gmail.com>
2021-11-12 20:54:40 +0300

    Добавлено UserValidation.

--- Lab4/UserValidation.Designer.cs
+++ Lab4/UserValidation.Designer.cs
@@ -36,19 +36,19 @@ namespace KurgasovLabs.Lab4
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 43);
+            this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Войти";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 107);
+            this.pictureBox1.Location = new System.Drawing.Point(12, 46);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(163, 99);
+            this.pictureBox1.Size = new System.Drawing.Size(160, 160);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
@@ -57,7 +57,7 @@ namespace KurgasovLabs.Lab4
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(216, 218);
+            this.ClientSize = new System.Drawing.Size(184, 218);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
             this.Name = "UserValidation";
--- Lab4/UserValidation.cs
+++ Lab4/UserValidation.cs
@@ -20,7 +20,29 @@ namespace KurgasovLabs.Lab4
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
+            var inputBoxName = new Interaction.InputBox("Введите ваше имя.");
+            var inputBoxPassword = new Interaction.InputBox("Введите пароль.");
+
+            if (inputBoxName.ShowDialog() != DialogResult.OK) return;
+            if (inputBoxPassword.ShowDialog() != DialogResult.OK) return;
+
+            string userName = inputBoxName.Result;
+            string password = inputBoxPassword.Result;
+
+            if (userName == "Генри" && password == "Цветок")
+            {
+                pictureBox1.Image = Properties.Resources.Henri;
+                MessageBox.Show("Добро пожаловать, Генри! Как поживаешь?");
+            }
+            else if (userName == "Феликс" && password == "Листок")
+            {
+                pictureBox1.Image = Properties.Resources.Feliks;
+                MessageBox.Show("Добро пожаловать, Феликс! Сыграем?");
+            }
+            else
+            {
+                MessageBox.Show("Извините, я вас не знаю.");
+            }
         }
     }
 }
