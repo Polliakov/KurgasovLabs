040ed872fd68beccd92232637980a54ba72da8d4
IRinaIvleva <iv.irina2003@mail.ru>
2021-11-14 20:47:58 +0300

    AccesModeChecker и удаление комментариев в FrameworkMath

--- Lab3/FrameworkMath.cs
+++ Lab3/FrameworkMath.cs
@@ -1,14 +1,5 @@
-﻿// КОМЕНТАРИИ УДАЛИТЬ ПЕРЕД ОТПРАВКОЙ
-
+﻿
 using System;
-// Не нужные using желательно удалять.
-//using System.Collections.Generic;
-//using System.ComponentModel;
-//using System.Data;
-//using System.Drawing;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace KurgasovLabs.Lab3
@@ -22,14 +13,6 @@ namespace KurgasovLabs.Lab3
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //double x = Convert.ToInt32(textBox1.Text);
-
-            /* Не отлавливается RuntimeExeption вызываемое Convert.ToInt32() при некорректном вводе строки.
-             * Для решения проблеммы можно использовать метод int.TryParse() или блоки try{} catch{}.
-             * 
-             * Приведение к int значения, предпологаемо типа double ( Convert.ToInt32() ).
-             * Следует использовать Convert.ToDouble() или double.TryParse().
-             */
             if (!double.TryParse(textBox1.Text, out double x))
             {
                 MessageBox.Show("Ошибка ввода!");
@@ -37,18 +20,6 @@ namespace KurgasovLabs.Lab3
             }
 
             double result = Math.Pow(x, 2);
-
-            //string result_str = Convert.ToString(result);
-            //MessageBox.Show(result_str, "Результат");
-
-            /* Ненужное объявление переменной result_str, переменная используется только 1 раз для вывода.
-             * 
-             * Примечание: Convert.ToString(result) и result.ToString() не имеют существенных различий,
-             * второй вариант имеет боле краткую форму и более читаем.
-             * 
-             * Нарушение нотации именования переменных. 
-             * Для локальных переменных нотация camelCase.
-             */
             MessageBox.Show(result.ToString(), "Результат");
         }
 
@@ -56,11 +27,5 @@ namespace KurgasovLabs.Lab3
         {
             Close();
         }
-
-        /* Графическая часть:
-         * Не нужно жмыхать кнопки (Зошто?).
-         * *Использование выравнивания упрощает создание ровного интерфейса,
-         * верхняя панель при выборе нескольких элементов.
-         */
     }
 }
--- Lab4/AccesModeChecker.cs
+++ Lab4/AccesModeChecker.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace KurgasovLabs.Lab4
@@ -15,6 +8,40 @@ namespace KurgasovLabs.Lab4
         public AccesModeChecker()
         {
             InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            switch (textBox1.Text)
+            {
+                case "9583": 
+                    label1.Text = "А, Б, В";
+                    break;
+                case "1747":
+                    label1.Text = "А, Б, В";
+                    break;
+                case "3331": 
+                    label1.Text = "Б, В";
+                    break;
+                case "7922": 
+                    label1.Text = "Б, В";
+                    break;
+                case "9455":
+                    label1.Text = "В";
+                    break;
+                case "8997":
+                    label1.Text = "В";
+                    break;
+                default:
+                    label1.Text = "У Вас нет доступа";
+                    break;
+            }
+
         }
     }
 }
