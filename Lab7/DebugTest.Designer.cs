178d1195a4fbe50e3cc3a6e11f833113ea8f6b4f
IRinaIvleva <iv.irina2003@mail.ru>
2021-11-14 21:24:23 +0300

    DebugTest дополлнение

--- Lab7/DebugTest.Designer.cs
+++ Lab7/DebugTest.Designer.cs
@@ -41,46 +41,52 @@ namespace KurgasovLabs.Lab7
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(53, 52);
+            this.textBox1.Location = new System.Drawing.Point(16, 51);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(151, 20);
+            this.textBox1.Size = new System.Drawing.Size(200, 22);
             this.textBox1.TabIndex = 0;
             // 
             // contextMenuStrip1
             // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.contextMenuStrip1.Name = "contextMenuStrip1";
             this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(53, 126);
+            this.textBox2.Location = new System.Drawing.Point(13, 126);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(151, 20);
+            this.textBox2.Size = new System.Drawing.Size(200, 22);
             this.textBox2.TabIndex = 2;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(50, 36);
+            this.label1.Location = new System.Drawing.Point(13, 18);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(99, 13);
+            this.label1.Size = new System.Drawing.Size(125, 17);
             this.label1.TabIndex = 3;
             this.label1.Text = "Cколько вам лет?";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(53, 107);
+            this.label2.Location = new System.Drawing.Point(13, 90);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.Size = new System.Drawing.Size(50, 17);
             this.label2.TabIndex = 4;
             this.label2.Text = "Вывод";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(253, 126);
+            this.button1.Location = new System.Drawing.Point(286, 126);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(100, 28);
             this.button1.TabIndex = 5;
             this.button1.Text = "Выход";
             this.button1.UseVisualStyleBackColor = true;
@@ -88,9 +94,10 @@ namespace KurgasovLabs.Lab7
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(253, 52);
+            this.button2.Location = new System.Drawing.Point(286, 45);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(100, 28);
             this.button2.TabIndex = 6;
             this.button2.Text = "Тест";
             this.button2.UseVisualStyleBackColor = true;
@@ -98,15 +105,16 @@ namespace KurgasovLabs.Lab7
             // 
             // DebugTest
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(513, 268);
+            this.ClientSize = new System.Drawing.Size(428, 189);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "DebugTest";
             this.Text = "DebugTest";
             this.ResumeLayout(false);
--- Lab7/DebugTest.cs
+++ Lab7/DebugTest.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace KurgasovLabs.Lab7
@@ -19,6 +12,12 @@ namespace KurgasovLabs.Lab7
 
         private void button2_Click(object sender, EventArgs e)
         {
+           if (textBox2.Text == "")
+            {
+                MessageBox.Show("Не заполнено!");
+                Close();
+            }
+           
             int Age;
             Age = Convert.ToInt32(textBox1.Text);
             if(Age >= 13 && Age <20){
@@ -28,7 +27,9 @@ namespace KurgasovLabs.Lab7
             {
                 textBox2.Text = "Вы не тинэйджер";
             }
+            
         }
+       
 
         private void button1_Click(object sender, EventArgs e)
         {
