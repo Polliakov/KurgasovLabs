1cc2e15603cbfe129307d3ec1e9d892866a112ed
IRinaIvleva <iv.irina2003@mail.ru>
2021-11-27 22:17:21 +0300

    Лаба 4. Внешний вид задания под вариантом 7

--- Lab4/UsingExample7.Designer.cs
+++ Lab4/UsingExample7.Designer.cs
@@ -29,12 +29,328 @@ namespace KurgasovLabs.Lab4
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.checkedListBox2 = new System.Windows.Forms.CheckedListBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.checkedListBox3 = new System.Windows.Forms.CheckedListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.BackColor = System.Drawing.Color.SeaShell;
+            this.checkedListBox1.CheckOnClick = true;
+            this.checkedListBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkedListBox1.ForeColor = System.Drawing.Color.LightCoral;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.HorizontalScrollbar = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "Жульен с курицей и грибами ",
+            "Тигровые креветки в манговом соусе ",
+            "Жареные креветки ",
+            "Сырное ассорти "});
+            this.checkedListBox1.Location = new System.Drawing.Point(12, 105);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(327, 89);
+            this.checkedListBox1.TabIndex = 0;
+            // 
+            // checkedListBox2
+            // 
+            this.checkedListBox2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.checkedListBox2.BackColor = System.Drawing.Color.SeaShell;
+            this.checkedListBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkedListBox2.ForeColor = System.Drawing.Color.LightCoral;
+            this.checkedListBox2.FormattingEnabled = true;
+            this.checkedListBox2.Items.AddRange(new object[] {
+            "Чай ",
+            "Сок ",
+            "Кофе ",
+            "Смузи ",
+            "Молочный коктель "});
+            this.checkedListBox2.Location = new System.Drawing.Point(419, 105);
+            this.checkedListBox2.Name = "checkedListBox2";
+            this.checkedListBox2.Size = new System.Drawing.Size(209, 89);
+            this.checkedListBox2.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.SeaShell;
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.ForeColor = System.Drawing.Color.LightCoral;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 18;
+            this.listBox1.Items.AddRange(new object[] {
+            "230",
+            "750",
+            "470",
+            "750"});
+            this.listBox1.Location = new System.Drawing.Point(345, 105);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(70, 94);
+            this.listBox1.TabIndex = 2;
+            // 
+            // listBox2
+            // 
+            this.listBox2.BackColor = System.Drawing.Color.SeaShell;
+            this.listBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox2.ForeColor = System.Drawing.Color.LightCoral;
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 18;
+            this.listBox2.Items.AddRange(new object[] {
+            "60",
+            "60",
+            "60",
+            "120",
+            "160"});
+            this.listBox2.Location = new System.Drawing.Point(636, 105);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(80, 94);
+            this.listBox2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.LightCoral;
+            this.label1.Location = new System.Drawing.Point(117, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(513, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Пожалуйста, выберите то, что хотите заказать!";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe Script", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.LightCoral;
+            this.label2.Location = new System.Drawing.Point(12, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 27);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Закуски";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe Script", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.LightCoral;
+            this.label3.Location = new System.Drawing.Point(340, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 27);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Цена";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe Script", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.Color.LightCoral;
+            this.label4.Location = new System.Drawing.Point(636, 75);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 27);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Цена";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe Script", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.ForeColor = System.Drawing.Color.LightCoral;
+            this.label5.Location = new System.Drawing.Point(430, 75);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(103, 27);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Напитки";
+            // 
+            // checkedListBox3
+            // 
+            this.checkedListBox3.BackColor = System.Drawing.Color.SeaShell;
+            this.checkedListBox3.CheckOnClick = true;
+            this.checkedListBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkedListBox3.ForeColor = System.Drawing.Color.LightCoral;
+            this.checkedListBox3.FormattingEnabled = true;
+            this.checkedListBox3.HorizontalScrollbar = true;
+            this.checkedListBox3.Items.AddRange(new object[] {
+            "Суп куриный",
+            "Борщ",
+            "Грибной крем - суп",
+            "Крем - суп с морепродуктами"});
+            this.checkedListBox3.Location = new System.Drawing.Point(12, 247);
+            this.checkedListBox3.Name = "checkedListBox3";
+            this.checkedListBox3.Size = new System.Drawing.Size(327, 89);
+            this.checkedListBox3.TabIndex = 9;
+            // 
+            // listBox3
+            // 
+            this.listBox3.BackColor = System.Drawing.Color.SeaShell;
+            this.listBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox3.ForeColor = System.Drawing.Color.LightCoral;
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 18;
+            this.listBox3.Items.AddRange(new object[] {
+            "190",
+            "230",
+            "270",
+            "350"});
+            this.listBox3.Location = new System.Drawing.Point(345, 247);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(70, 94);
+            this.listBox3.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe Script", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.ForeColor = System.Drawing.Color.LightCoral;
+            this.label6.Location = new System.Drawing.Point(340, 217);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(58, 27);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Цена";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe Script", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.ForeColor = System.Drawing.Color.LightCoral;
+            this.label7.Location = new System.Drawing.Point(12, 217);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(61, 27);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Супы";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Wheat;
+            this.button1.Location = new System.Drawing.Point(12, 347);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(704, 17);
+            this.button1.TabIndex = 13;
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe Script", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.ForeColor = System.Drawing.Color.LightCoral;
+            this.label8.Location = new System.Drawing.Point(12, 378);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(185, 32);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Сумма заказа:";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.SeaShell;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.ForeColor = System.Drawing.Color.LightCoral;
+            this.button2.Location = new System.Drawing.Point(572, 300);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(135, 36);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Готово";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.SeaShell;
+            this.textBox1.ForeColor = System.Drawing.Color.LightCoral;
+            this.textBox1.Location = new System.Drawing.Point(18, 413);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(179, 22);
+            this.textBox1.TabIndex = 16;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.SeaShell;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.ForeColor = System.Drawing.Color.LightCoral;
+            this.button3.Location = new System.Drawing.Point(17, 441);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(135, 36);
+            this.button3.TabIndex = 17;
+            this.button3.Text = "Согласен";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.SeaShell;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.ForeColor = System.Drawing.Color.LightCoral;
+            this.button4.Location = new System.Drawing.Point(18, 483);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(135, 48);
+            this.button4.TabIndex = 18;
+            this.button4.Text = "Отменить и \r\nвыйти";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // UsingExample7
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.OldLace;
+            this.ClientSize = new System.Drawing.Size(719, 583);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.checkedListBox3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.checkedListBox2);
+            this.Controls.Add(this.checkedListBox1);
+            this.ForeColor = System.Drawing.Color.LightCoral;
+            this.Name = "UsingExample7";
             this.Text = "UsingExample7";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.CheckedListBox checkedListBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckedListBox checkedListBox3;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
